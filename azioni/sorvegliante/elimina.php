74a6a8f6b05af6c1d85d9eeb82ea0fc2866f3f7f
Giustino Borzacchiello <giustinob@gmail.com>
2010-12-22 12:24:58 +0100

    sistemata l'eliminazione di un sorvegliante

--- azioni/sorvegliante/elimina.php
+++ azioni/sorvegliante/elimina.php
@@ -1,12 +1,33 @@
 <?php
 require_once 'config.php';
 require_once 'Sorvegliante.php';
-$id = '';
-extract($_GET, EXTR_IF_EXISTS);
-$s = Sorvegliante::find_by_id($id);
-$s->delete();
-unset($s);
-header("Location: ".$publicUrl."/ASC/sorveglianti/");
+require_once 'Validator.php';
+require_once 'Redirect.php';
+
+$v = new Validator($_POST);
+
+$v->isNotEmpty('elimina');
+$v->isNotEmpty('matricola');
 
 
+$e = $v->getError();
+
+if (empty($e)) {
+	$clean = $v->getClean();
+	if ($clean['elimina'] === 'SI') {
+		//cancella sorvegliante
+		//recupera sorvegliante dal db
+		$s = Sorvegliante::find_by_id($clean['matricola']);
+		//eliminalo
+		$s->delete();
+	}
+	//redirect all'index dei sorveglianti
+	$r = new Redirect(PUBLIC_URL . "/ASC/sorveglianti/");
+	$r->doRedirect();
+} else {
+	//errori - possibile intrusione?
+	//@todo creare la pagina di errore generale
+	$r = new Redirect('error.php');
+	$r->doRedirect();
+}
 ?>
--- public/ASC/sorveglianti/elimina.php
+++ public/ASC/sorveglianti/elimina.php
@@ -1,9 +1,42 @@
 <?php
-//echo "Il referer è " . $_SERVER['HTTP_REFERER'];
-if ( is_numeric($_GET['id']) ) {
-	$id = $_GET['id'];
-} else {
-	die('Non dovresti essere qui!');
+require_once 'config.php';
+require_once 'Sorvegliante.php';
+require_once 'Validator.php';
+require_once 'Redirect.php';
+
+$pageTitle = 'Elimina sorvegliante';
+
+$v = new Validator($_GET);
+$v->isNotEmpty('matricola');
+$v->isNumeric('matricola');
+//@todo fare decodeurl su matricola
+$e = $v->getError();
+if(!empty($e)){
+	//@todo creare la pagina di errore generale
+	$r = new Redirect('error.php');
+	$r->doRedirect();
 }
-echo "<p>Vuoi cancellare il sorvegliante con matricola $id</p>";
+
+$clean = $v->getClean();
+$s = Sorvegliante::find_by_id($clean['matricola']);
+$eliminaUrl = ACTION_URL . '/sorvegliante/elimina.php';
 ?>
+<!doctype html>
+<html>
+   <head>
+      <title><?php echo $pageTitle; ?></title>
+   </head>
+   <body>
+		<h1><?php echo $pageTitle; ?></h1>
+		<p>Sei sicuro di voler cancellare il sorvegliante "<?php echo $s; ?>"?</p>
+		<form action="<?php echo $eliminaUrl; ?>" method="post">
+			<p><input type="radio" name="elimina" id="eliminaSI" value="SI" />Si</p>
+			<p><input type="radio" name="elimina" id="eliminaNO" value="NO" checked="checked" />No</p>
+			<p>
+				<input id="submit" name="submit" type="submit" value="Invia" />
+				<input type="hidden" name="matricola" value="<?php echo $s->getMatricola(); ?>" />
+			</p>
+		</form>
+
+	</body>
+</html>
