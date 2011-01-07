24a69ddcbb35ae1977f2f995ed7ac0037d8ef760
Giustino Borzacchiello <giustinob@gmail.com>
2011-01-07 12:48:15 +0100

    rinominato marker.js in creaPercorso.js

--- public/ATS/percorsi/nuovo.php
+++ public/ATS/percorsi/nuovo.php
@@ -51,5 +51,5 @@ $aggiungiUrl = ACTION_URL . '/percorso/aggiungi.php';
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>/lib/common.js"></script>
 	
-	<script type="text/javascript" src="marker.js"></script>
+	<script type="text/javascript" src="creaPercorso.js"></script>
 <?php include HELPERS_DIR . '/piepagina.php'; ?>
