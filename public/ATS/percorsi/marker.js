b636542edb95595f8c90b93ab452badf66cec3d0
Giustino Borzacchiello <giustinob@gmail.com>
2011-01-07 12:41:26 +0100

    collegato file common.js anche per il nuovo percorso

--- public/ATS/percorsi/marker.js
+++ public/ATS/percorsi/marker.js
@@ -8,19 +8,19 @@ function creaDivMappa() {
     document.body.appendChild(mapdiv);
     return mapdiv;
 }
-/**
- * Inizializza una mappa centrata su napoli
- */
-function initialize(mapDiv) {
-    var latlng = new google.maps.LatLng(40.847736,14.2519);
-    var myOptions = {
-        zoom: 10,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    return new google.maps.Map(mapDiv,
-        myOptions);
-}
+///**
+// * Inizializza una mappa centrata su napoli
+// */
+//function initialize(mapDiv) {
+//    var latlng = new google.maps.LatLng(40.847736,14.2519);
+//    var myOptions = {
+//        zoom: 10,
+//        center: latlng,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//    return new google.maps.Map(mapDiv,
+//        myOptions);
+//}
 
 $(document).ready(function(){
     //var mapDiv = creaDivMappa();
--- public/ATS/percorsi/nuovo.php
+++ public/ATS/percorsi/nuovo.php
@@ -10,7 +10,7 @@ $queryStr = "SELECT codicePC, indirizzo, latitudine, longitudine FROM PuntiDiCon
 $result = $db->query($queryStr);
 $pdc = array();
 while ($row = $result->fetch_assoc()) {
-    $pdc[] = $row;
+	$pdc[] = $row;
 }
 
 $aggiungiUrl = ACTION_URL . '/percorso/aggiungi.php';
@@ -23,31 +23,33 @@ $aggiungiUrl = ACTION_URL . '/percorso/aggiungi.php';
 <div id="map"></div>
 <div id="pdc">
     <select id="luoghi" size="10">
-        <?php foreach ($pdc as $luogo): ?>
-            <option value="<?php echo $luogo['latitudine'] . ',' . $luogo['longitudine'] . ',' . $luogo['codicePC']; ?>"><?php
-            echo trim($luogo['indirizzo']);
-        ?></option>
-        <?php endforeach; ?>
-        </select>
-    </div>
-    <div id="percorsoWrap">
-        <table id="percorso">
-            <thead>
-                <tr>
-                    <th>Indirizzo</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+		<?php foreach ($pdc as $luogo): ?>
+			<option value="<?php echo $luogo['latitudine'] . ',' . $luogo['longitudine'] . ',' . $luogo['codicePC']; ?>"><?php
+			echo trim($luogo['indirizzo']);
+		?></option>
+		<?php endforeach; ?>
+		</select>
+	</div>
+	<div id="percorsoWrap">
+		<table id="percorso">
+			<thead>
+				<tr>
+					<th>Indirizzo</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
 
-        <form id="salvaPercorso" action="<?php echo $aggiungiUrl; ?>" method="post">
-            <p><input type="submit" value="Salva Percorso" name="salvaPercorso" /></p>
-        </form>
+		<form id="salvaPercorso" action="<?php echo $aggiungiUrl; ?>" method="post">
+			<p><input type="submit" value="Salva Percorso" name="salvaPercorso" /></p>
+		</form>
 
-    </div>
+	</div>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-<script type="text/javascript" src="marker.js"></script> 
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script type="text/javascript" src="<?php echo BASE_URL; ?>/lib/common.js"></script>
+	
+	<script type="text/javascript" src="marker.js"></script>
 <?php include HELPERS_DIR . '/piepagina.php'; ?>
