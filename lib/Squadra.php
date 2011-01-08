150faca98ad25182d18a469598893a6ff5e26966
Giustino Borzacchiello <giustinob@gmail.com>
2011-01-08 10:27:17 +0100

    terminata la pagina di dettaglio dei sorveglianti

--- lib/Squadra.php
+++ lib/Squadra.php
@@ -7,6 +7,17 @@
  */
 class Squadra {
 	private static $nomeTabella = 'Squadre';
+	public static function find_by_id($id){
+		$db = DB::getInstance();
+		$queryStr = "SELECT codiceSquadra,nomeSquadra from " . self::$nomeTabella . " WHERE codiceSquadra=$id";
+		try {
+			$out = $db->fetchFirst($queryStr);
+			return $out;
+		} catch (DatabaseErrorException $e) {
+			echo __FILE__ . "Impossibile eseguire la query";
+			exit;
+		}
+	}
 
 	public static function findAll() {
 		$db = DB::getInstance();
--- public/ASC/sorveglianti/dettagli.php
+++ public/ASC/sorveglianti/dettagli.php
@@ -1,7 +1,36 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'config.php';
+require_once 'Sorvegliante.php';
+require_once 'Validator.php';
+require_once 'Redirect.php';
+require_once 'Squadra.php';
 
+
+$pageTitle = "Dettagli Sorvegliante";
+
+$v = new Validator($_GET);
+//@todo fare decodeurl su matricola
+$v->isNotEmpty('matricola');
+$v->isNumeric('matricola');
+
+$e = $v->getError();
+if (!empty($e)) {
+	//@todo creare la pagina di errore generale
+	$r = new Redirect('error.php');
+	$r->doRedirect();
+}
+
+$clean = $v->getClean();
+$s = Sorvegliante::find_by_id($clean['matricola']);
+$squadra = Squadra::find_by_id($s->getCodiceSquadra());
 ?>
+<?php include HELPERS_DIR . '/testata.php'; ?>
+<h1><?php echo $pageTitle; ?></h1>
+<ul>
+	<li>Matricola: <?php echo $s->getMatricola(); ?></li>
+	<li>Nome: <?php echo $s->getNome(); ?></li>
+	<li>Cognome: <?php echo $s->getCognome(); ?></li>
+	<li>Squadra: <?php echo $squadra['nomeSquadra']; ?></li>
+</ul>
+<p><a href="../sorveglianti/">Indietro</a></p>
+<?php include HELPERS_DIR . '/piepagina.php'; ?>
