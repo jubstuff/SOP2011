0f64d2faa0b628517c57063c79d831ff3f44b4cd
Giustino Borzacchiello <giustinob@gmail.com>
2011-01-11 18:34:44 +0100

    aggiunto form di login

--- public/index.php
+++ public/index.php
@@ -3,12 +3,34 @@ require_once 'config.php';
 $pageTitle = "Sistema Informativo SOP 2011";
 ?>
 <?php include HELPERS_DIR . '/testata.php'; ?>
-
 <h1><?php echo $pageTitle; ?></h1>
+<!--
 <p>Cosa vuoi fare?</p>
 <ul>
 	<li><a href="ASC/">Amministrare clienti e sorveglianti</a></li>
 	<li><a href="ATS/">Amministrare turni e percorsi</a></li>
 	<li><a href="Sorvegliante/">[Sorvegliante] Visualizzare i miei turni e percorsi</a></li>
-</ul>
+</ul>-->
+
+	<form action="<?php echo ACTION_URL; ?>/utente/login.php" method="post">
+		<p>
+			<label for="username">Username</label>
+			<input id="username" name="username" type="text" />
+		</p>
+		<p>
+			<label for="password">Password</label>
+			<input id="password" name="password" type="password" />
+		</p>
+		<p>
+			<label for="ruolo">Ruolo</label>
+			<select id="ruolo" name="ruolo">
+				<option value="ASC">Amministratore sorveglianti e clienti</option>
+				<option value="ATS">Amministratore turni e percorsi</option>
+				<option value="SOR">Sorvegliante</option>
+			</select>
+		</p>
+		<p>
+			<input id="submit" name="submit" type="submit" value="Login" />
+		</p>
+	</form>
 <?php include HELPERS_DIR . '/piepagina.php'; ?>
