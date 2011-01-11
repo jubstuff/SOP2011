af435b923872d93f60ac38273e9746411d89296f
Giustino Borzacchiello <giustinob@gmail.com>
2011-01-11 09:58:39 +0100

    modificato estraiInfo.php

--- utilita/provaSimpleXML0.4/estraiInfo.php
+++ utilita/provaSimpleXML0.4/estraiInfo.php
@@ -1,8 +1,8 @@
 <?php
+
 require_once 'config.php';
-require_once 'Sorvegliante.php';
+
 $pageTitle = "Estrai indirizzi";
-$tuttiISorveglianti = Sorvegliante::findAll();
 ?>
 
 <?php
@@ -11,33 +11,50 @@ $tuttiISorveglianti = Sorvegliante::findAll();
 $kml = simplexml_load_file('percorsiKMLfile.php');
 
 $info = $kml->document;
-//var_dump($doc_kml);
-
-//header("Content-type: application/json");
 
 /* Recupero dati KML */
-$infoAssoc = Array();
+$infoAssoc = array();
+//var_dump($doc_kml);
+//header("Content-type: application/json");
 
 //echo '<p>Informazioni recuperate dal kml</p>';
-for ($i = 0; $kml->document->data[$i]->percorso != ''; $i++) {
-  $infoAssoc['percorso'][$i] = $kml->document->data[$i]->percorso;
-  $infoAssoc['indirizzo'][$i] = $kml->document->data[$i]->indirizzo;
-  $infoAssoc['latitudine'][$i] = $kml->document->data[$i]->latitudine;
-  $infoAssoc['longitudine'][$i] = $kml->document->data[$i]->longitudine;
-  
+//for ($i = 0; $kml->document->data[$i]->percorso != ''; $i++) {
+//  $infoAssoc['percorso'][$i] = $kml->document->data[$i]->percorso;
+//  $infoAssoc['indirizzo'][$i] = $kml->document->data[$i]->indirizzo;
+//  $infoAssoc['latitudine'][$i] = $kml->document->data[$i]->latitudine;
+//  $infoAssoc['longitudine'][$i] = $kml->document->data[$i]->longitudine;
+//
+//}
+
+foreach ($kml->document->data as $d) {
+//	var_dump($d);
+	$codP = (int)$d->percorso;
+	$infoAssoc[$codP]['pdc'][] = array('indirizzo' => (string)$d->indirizzo, 'latitudine' => (float)$d->latitudine, 'longitudine' => (float)$d->longitudine);
 }
 
-$lunghezza = count($infoAssoc['percorso']);
+//print_r($infoAssoc);
+
+//$lunghezza = count($infoAssoc['percorso']);
 //echo 'lunghezza: ' . $lunghezza . '<br /><br />';
 /*
-for ($i = 0; $i < $lunghezza; $i++) {
-	echo $infoAssoc['percorso'][$i] . '<br />';
-	echo $infoAssoc['indirizzo'][$i] . '<br />';
-	echo $infoAssoc['latitudine'][$i] . '<br />';
-	echo $infoAssoc['longitudine'][$i] . '<br /><br />';
+  for ($i = 0; $i < $lunghezza; $i++) {
+  echo $infoAssoc['percorso'][$i] . '<br />';
+  echo $infoAssoc['indirizzo'][$i] . '<br />';
+  echo $infoAssoc['latitudine'][$i] . '<br />';
+  echo $infoAssoc['longitudine'][$i] . '<br /><br />';
 
-}
-*/
+  }
+ */
 echo json_encode($infoAssoc);
-
+//
+//
+//
+//
+//$arrayDiProva = array(
+//	array('percorso' => 1, 'pdc' => array(
+//		array('indirizzo' => 'via umbria napoli', 'latitudine' => 40.89354, 'longitudine' => 14.258068),
+//		array('indirizzo' => 'via toledo napoli', 'latitudine' => 40.845509, 'longitudine' => 14.249201),
+//	))
+//);
+//echo '<p>',json_encode($arrayDiProva),'</p>';
 ?>
